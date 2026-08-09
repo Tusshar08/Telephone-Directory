a4a18010e51fdf08744bf0dddbd68faa73c7fc68
Tusshar08 <tussharraj08@gmail.com>
2026-08-09 18:20:22 +0530

    Added feature to write table structure and fail reason to  output

--- Directory.java
+++ Directory.java
@@ -7,6 +7,7 @@ public class Directory {
 
     private static final int TABLES = 9; // Total tables
     private static final int TABLE_SIZE = 20; // Slots in each table
+    private String fail = ""; // Output failure reason
 
     static class Record {
 
@@ -66,23 +67,27 @@ public class Directory {
     private boolean validPhone(String phone){
 
         if(phone ==  null || phone.length() !=7){
-            System.out.println("Invalid Number Length");
+            fail = "Invalid Number Length";
             return false;
         }
 
         for(char ch : phone.toCharArray()){
             if(!Character.isDigit(ch)){
-                System.out.println("Invalid Number: Has Other Characters");
+                fail = "Invalid Number: Has Other Characters";
                 return false;
             }
         }
 
         if(phone.charAt(0) == '0'){
-            System.out.println("Invalid First Digit");
+            fail = "Invalid First Digit";
             return false;
         }
 
         return true;
+    }
+
+    public String getFail(){
+        return fail;
     }
 
     public boolean insert(String name, String phone, String address) {
@@ -92,7 +97,7 @@ public class Directory {
         int index = getTableIndex(phone); // Find table
         int hashValue = hash(phone); // Compute hash
         if(search(phone,index,hashValue)!=-1){  // Check duplicate phone
-            System.out.println("Phone already exists.");
+            fail = "Phone number already exists";
             return false;
         }
 
@@ -105,7 +110,7 @@ public class Directory {
             }
         }
 
-        System.out.println("Table's full");
+        fail = "Table's full";
         return false; // Insertion Unsuccessful
     }
 
@@ -117,7 +122,7 @@ public class Directory {
         int slot = search(phone,index,hashValue);
         
         if(slot == -1){
-            System.out.println("Record unavailable");
+            fail = "Record unavailable";
             return false;
         }
 
@@ -134,7 +139,7 @@ public class Directory {
         int slot = search(oldPhone,index,hashValue);
         
         if(slot == -1){
-            System.out.println("Record unavailbale");
+            fail = "Record unavailbale";
             return false;
         }
 
@@ -150,7 +155,7 @@ public class Directory {
             int newIndex = getTableIndex(newPhone); // Find table
             int newHashValue = hash(newPhone); // Compute hash
             if(search(newPhone,newIndex,newHashValue)!=-1){  // Check duplicate phone
-                System.out.println("Phone already exists.");
+                fail = "Phone already exists.";
                 return false;
             }
 
@@ -181,19 +186,19 @@ public class Directory {
         return -1;
     }
 
-    public void display() {
+    public void display(PrintWriter out) {
         for (int i = 0; i < TABLES; i++) {
-            System.out.println("\n Table T" + (i + 1));
+            out.println("\n Table T" + (i + 1));
             for (int j = 0; j < TABLE_SIZE; j++) {
-                System.out.print("Slot " + j + " : ");
+                out.print("Slot " + j + " : ");
                 if (tables[i][j].status == Status.EMPTY) {
-                    System.out.println("EMPTY");
+                    out.println("EMPTY");
                 }
                 else if (tables[i][j].status == Status.DELETED) {
-                    System.out.println("DELETED");
+                    out.println("DELETED");
                 }
                 else {
-                    System.out.println( "Name: " + tables[i][j].name + ", Phone: " + tables[i][j].phone + ", Address: " + tables[i][j].address );
+                    out.println( "Name: " + tables[i][j].name + ", Phone: " + tables[i][j].phone + ", Address: " + tables[i][j].address );
                 }
             }
         }
@@ -215,13 +220,13 @@ public class Directory {
 
                 case "insert":{
                     boolean inserted = directory.insert(section[1], section[2], section[3]);
-                    out.println(inserted);
+                    out.println(inserted ? "Success" : "Failure: " + directory.getFail());
 
                     break;
                 }
                 case "delete":{
                     boolean delete = directory.delete(section[1]);
-                    out.println(delete);
+                    out.println(delete ? "Success" : "Failure: " + directory.getFail());
 
                     break;
                 }
@@ -232,14 +237,13 @@ public class Directory {
                         newPhone = section[3];
                     }
                     boolean modified = directory.modify(section[1], section[2], newPhone);
-                    out.println(modified);
+                    out.println(modified ? "Success" : "Failure: " + directory.getFail());
 
                     break;
                 }
 
-                case "debug":
-
-                    directory.display();
+                case "print":
+                    directory.display(out);
 
                     break;
             }
